f1b9c5f37988b55f43d8637847f6b28f28768c3f
Vinicius Fidelis Da Cruz <vfidelis@c2r5s3.42.rio>
2025-04-18 01:15:34 -0300

    first main

---
+++ main.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vfidelis <vfidelis@student.42.rio>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/16 21:06:12 by vfidelis          #+#    #+#             */
+/*   Updated: 2025/04/16 21:48:04 by vfidelis         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minihell.h"
+
+int	main()
+{
+	char *input;
+	
+	while (1)
+	{
+		input = readline("minishell$ ");
+		if (!input)
+		{
+			printf("exit\n");
+			break;
+		}
+		if (*input)
+			add_history(input);
+		printf("Você digitou: %s\n", input);
+		free(input);
+	}
+}
