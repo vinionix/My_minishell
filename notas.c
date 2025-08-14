a6c9e8d122319ba953424b1c0ed96ffb92151550
Gabriel Salgado <elementalgames77@gmail.com>
2025-08-14 01:33:04 -0300

    fix/features: coloquei muita coisa na norma, consertei a parada do erro indo pro pipe em vez de ir pro stdout, coloquei a expansão no heredoc e consertei um problema no export sem parametros que estava printando o exit code e o fake pid

--- notas.c
+++ notas.c
@@ -1,7 +1,7 @@
 O que falta?
 
 1 - Refatorar e deixar tudo na norma ().
-2 - Erro e free em caso de comando inexistente ().
+//2 - Erro e free em caso de comando inexistente ().
 3 - Erro no valid_path ().
 //4 - Control c e Control \(X).
 //5 - Colocar exit code na variavel de ambiente(X).
